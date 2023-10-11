8ee7d2ff9a076d04d8896927d3d8cacab32cbc3e
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2023-10-11 19:16:59 -0400

    correct

--- benchmark_context.sh
+++ benchmark_context.sh
@@ -20,11 +20,11 @@ module load StdEnv/2020
 module load python/3.10
 module load scipy-stack
 module load gurobi
-source /home/mheuill/projects/def-adurand/mheuill/MYENV2/bin/activate
+source /home/mheuill/projects/def-adurand/mheuill/MYENV3/bin/activate
 
-virtualenv-clone /home/mheuill/projects/def-adurand/mheuill/MYENV2 $SLURM_TMPDIR/MYENV2
+virtualenv-clone /home/mheuill/projects/def-adurand/mheuill/MYENV3 $SLURM_TMPDIR/MYENV2
 deactivate
-source $SLURM_TMPDIR/MYENV2/bin/activate
+source $SLURM_TMPDIR/MYENV3/bin/activate
 
 echo 'HZ: start python3 ./experiment.py ..at '; date
 
