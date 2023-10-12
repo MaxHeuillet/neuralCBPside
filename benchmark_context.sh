77854627800ca804d0ab8498b69994b68364e263
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2023-10-11 21:49:46 -0400

    swap to pulp

--- benchmark_context.sh
+++ benchmark_context.sh
@@ -19,7 +19,7 @@ module --force purge
 module load StdEnv/2020
 module load python/3.10
 module load scipy-stack
-module load gurobi
+#module load gurobi
 source /home/mheuill/projects/def-adurand/mheuill/MYENV3/bin/activate
 
 virtualenv-clone /home/mheuill/projects/def-adurand/mheuill/MYENV3 $SLURM_TMPDIR/MYENV2
