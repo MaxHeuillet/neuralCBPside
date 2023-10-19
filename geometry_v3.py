f5f74aabf27cd5a058a8582726ce9b6a109f6bab
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2023-10-19 17:45:43 -0400

    update

--- geometry_v3.py
+++ geometry_v3.py
@@ -109,43 +109,47 @@ def alphabet_size(FeedbackMatrix, N,M):
 
 def solve_LP(args):
     z, M, LossMatrix, halfspace = args
+    with gp.Env(empty=True) as env:
+        env.setParam('OutputFlag', 0)
+        env.start()
+        with gp.Model(env=env) as m:
 
-    # Create a new model
-    m = gp.Model() #name="Pareto_Optimization_{}".format(z)
-    m.Params.LogToConsole = 0
-    
-    m.setParam("OutputFlag", 0)  # turn off Gurobi's output
-    #print('n threads', m.Params.Threads)
-    #m.setParam("Threads", 1)
-    #print('n threads', m.Params.Threads)
+            # Create a new model
+            #m = gp.Model() #name="Pareto_Optimization_{}".format(z)
+            m.Params.LogToConsole = 0
+            
+            m.setParam("OutputFlag", 0)  # turn off Gurobi's output
+            #print('n threads', m.Params.Threads)
+            #m.setParam("Threads", 1)
+            #print('n threads', m.Params.Threads)
 
-    # Add variables
-    vars = m.addVars(M, lb=0.00001, ub=1.0, vtype=gp.GRB.CONTINUOUS, name='p')
+            # Add variables
+            vars = m.addVars(M, lb=0.00001, ub=1.0, vtype=gp.GRB.CONTINUOUS, name='p')
 
-    # Set objective (in this case, no objective is specified, so we'll set it to 0)
-    m.setObjective(0, gp.GRB.MINIMIZE)
+            # Set objective (in this case, no objective is specified, so we'll set it to 0)
+            m.setObjective(0, gp.GRB.MINIMIZE)
 
-    # Add constraint: sum(vars) == 1.0
-    m.addConstr(sum(vars[i] for i in range(M)) == 1.0, "css")
+            # Add constraint: sum(vars) == 1.0
+            m.addConstr(sum(vars[i] for i in range(M)) == 1.0, "css")
 
-    # Add loss constraints
-    lossExprs = np.dot(LossMatrix - LossMatrix[z, :], [vars[i] for i in range(M)])
-    for i2, lossExpr in enumerate(lossExprs):
-        if i2 != z:
-            m.addConstr(lossExpr >= 0.0, 'c_{}'.format(i2))
+            # Add loss constraints
+            lossExprs = np.dot(LossMatrix - LossMatrix[z, :], [vars[i] for i in range(M)])
+            for i2, lossExpr in enumerate(lossExprs):
+                if i2 != z:
+                    m.addConstr(lossExpr >= 0.0, 'c_{}'.format(i2))
 
-    # Add halfspace constraints
-    for element in halfspace:
-        pair, sign = element
-        if sign != 0:
-            halfspaceExpr = sum(sign * (LossMatrix[pair[0]][j] - LossMatrix[pair[1]][j]) * vars[j] for j in range(M))
-            m.addConstr(halfspaceExpr >= 0.001, "ch_{}_{}".format(pair[0], pair[1]))
+            # Add halfspace constraints
+            for element in halfspace:
+                pair, sign = element
+                if sign != 0:
+                    halfspaceExpr = sum(sign * (LossMatrix[pair[0]][j] - LossMatrix[pair[1]][j]) * vars[j] for j in range(M))
+                    m.addConstr(halfspaceExpr >= 0.001, "ch_{}_{}".format(pair[0], pair[1]))
 
-    # Solve the model
-    m.optimize()
+            # Solve the model
+            m.optimize()
 
-    if m.status == gp.GRB.OPTIMAL:
-        return z
+            if m.status == gp.GRB.OPTIMAL:
+                return z
     return None
 
 def getParetoOptimalActions(LossMatrix, N, M, halfspace, num_pools=None):
@@ -173,41 +177,51 @@ def getNeighborhoodActions(LossMatrix, N, M, halfspace, mathcal_N, num_pools=Non
     return actions
 
 def isNeighbor(LossMatrix, N, M, i1, i2, halfspace):
-    m = gp.Model() #name="Neighbor_Check"
-    m.Params.LogToConsole = 0
-    lp.LpSolverDefault.msg = 0
-    m.setParam("OutputFlag", 0)  # Turn off Gurobi's output
-    #print('n threads', m.Params.Threads)
-    #m.setParam("Threads", 1)
-    #print('n threads', m.Params.Threads)
+    
+    with gp.Env(empty=True) as env:
+        env.setParam('OutputFlag', 0)
+        env.start()
+        with gp.Model(env=env) as m:
 
-    # Define variables
-    vars = m.addVars(M, lb=0.00001, ub=1.0, vtype=gp.GRB.CONTINUOUS, name='p')
+            m.Params.LogToConsole = 0
+            lp.LpSolverDefault.msg = 0
+            m.setParam("OutputFlag", 0)  # Turn off Gurobi's output
+            #print('n threads', m.Params.Threads)
+            #m.setParam("Threads", 1)
+            #print('n threads', m.Params.Threads)
 
-    # Add simplex constraint
-    m.addConstr(sum(vars[j] for j in range(M)) == 1.0, "css")
+            # Define variables
+            vars = m.addVars(M, lb=0.00001, ub=1.0, vtype=gp.GRB.CONTINUOUS, name='p')
 
-    # Add two-degenerate constraint
-    twoDegenerateExpr = sum((LossMatrix[i2][j] - LossMatrix[i1][j]) * vars[j] for j in range(M))
-    m.addConstr(twoDegenerateExpr == 0.0, "cdeg")
+            # Add simplex constraint
+            m.addConstr(sum(vars[j] for j in range(M)) == 1.0, "css")
 
-    # Add loss constraints
-    for i3 in range(N):
-        if i3 != i1:
-            lossExpr = sum((LossMatrix[i3][j] - LossMatrix[i1][j]) * vars[j] for j in range(M))
-            m.addConstr(lossExpr >= 0.0, "c_{}".format(i3))
+            # Add two-degenerate constraint
+            twoDegenerateExpr = sum((LossMatrix[i2][j] - LossMatrix[i1][j]) * vars[j] for j in range(M))
+            m.addConstr(twoDegenerateExpr == 0.0, "cdeg")
 
-    # Add halfspace constraints
-    for element in halfspace:
-        pair, sign = element[0], element[1]
-        if sign != 0:
-            halfspaceExpr = sum(
-                sign * (LossMatrix[pair[0]][j] - LossMatrix[pair[1]][j]) * vars[j] for j in range(M)
-            )
-            m.addConstr(halfspaceExpr >= 0.001, "ch_{}_{}".format(pair[0], pair[1]))
+            # Add loss constraints
+            for i3 in range(N):
+                if i3 != i1:
+                    lossExpr = sum((LossMatrix[i3][j] - LossMatrix[i1][j]) * vars[j] for j in range(M))
+                    m.addConstr(lossExpr >= 0.0, "c_{}".format(i3))
 
-    m.optimize()
-    return m.status == gp.GRB.OPTIMAL
+            # Add halfspace constraints
+            for element in halfspace:
+                pair, sign = element[0], element[1]
+                if sign != 0:
+                    halfspaceExpr = sum(
+                        sign * (LossMatrix[pair[0]][j] - LossMatrix[pair[1]][j]) * vars[j] for j in range(M)
+                    )
+                    m.addConstr(halfspaceExpr >= 0.001, "ch_{}_{}".format(pair[0], pair[1]))
+
+            m.optimize()
+            if m.status == gp.GRB.OPTIMAL:
+                result = True
+            else: 
+                result = False
+
+    return result
 
 
 
