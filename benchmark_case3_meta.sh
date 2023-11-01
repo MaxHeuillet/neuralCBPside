d1c568117e7029c1f490874144445f931d580244
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2023-11-01 12:24:39 -0400

    update

--- benchmark_case3_meta.sh
+++ benchmark_case3_meta.sh
@@ -12,7 +12,7 @@ for context_type in  'MNIST' #'linear' 'quadratic' 'sinusoid'
 
         do
 
-            for approach in 'EEneuralcbpside' # 'random' 'random2' 'ineural' 
+            for approach in 'EEneuralcbpside' 'random' 'random2' 'ineural' 
                     
                 do
 
