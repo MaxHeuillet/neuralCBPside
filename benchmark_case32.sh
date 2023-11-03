a0144f82863d3456b6060ed633a0070faebef0bd
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2023-11-02 21:41:39 -0400

    update

--- benchmark_case32.sh
+++ benchmark_case32.sh
@@ -20,11 +20,12 @@ module --force purge
 module load StdEnv/2020
 module load python/3.10
 module load scipy-stack
-module load gurobi
+# module load gurobi
 
+source /home/mheuill/projects/def-adurand/mheuill/MYENV3/bin/activate
 
 #source /home/mheuill/projects/def-adurand/mheuill/ENV_nogurobi/bin/activate
-source /home/mheuill/projects/def-adurand/mheuill/Gurobi_Py310/bin/activate
+# source /home/mheuill/projects/def-adurand/mheuill/Gurobi_Py310/bin/activate
 
 # virtualenv-clone /home/mheuill/projects/def-adurand/mheuill/MYENV3 $SLURM_TMPDIR/MYENV2
 # deactivate
