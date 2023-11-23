da0fae95128933be92c9ad70e265fccb7db15a28
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2023-11-23 10:45:09 -0500

    update

--- benchmark_meta.sh
+++ benchmark_meta.sh
@@ -6,26 +6,20 @@ context_type=$2
 case=$3
 model=$4
 
-if [ "$case" == "case1" ]; then
-    approaches=( 'neuronal3'  'neuronal6' 'EEneuralcbpside_v6') #'margin' 'cesa' 'ineural3' 'ineural6'  'EEneuralcbpside_v6' 'EEneuralcbpside_v5' 'ineural3' 'ineural6' 'neuronal3' 'neuronal6' 
-else   
-    approaches=(  'neuronal3'  'neuronal6' 'EEneuralcbpside_v6') #'ineural3' 'ineural6' 'EEneuralcbpside_v6' 'EEneuralcbpside_v5'  'ineural3' 'ineural6' 'neuronal3' 'neuronal6' 
+
+if [ "$model" == "MLP" ]; then
+    if [ "$case" == "case1" ]; then
+        approaches=('margin' 'cesa' 'ineural3' 'ineural6' 'neuronal3' 'neuronal6' 'EEneuralcbpside_v6')
+    else
+        approaches=('ineural3' 'ineural6' 'neuronal3' 'neuronal6' 'EEneuralcbpside_v6')
+    fi
+else
+    if [ "$case" == "case1" ]; then
+        approaches=('neuronal3' 'neuronal6' 'EEneuralcbpside_v6')
+    else
+        approaches=('neuronal3' 'neuronal6' 'EEneuralcbpside_v6')
+    fi
 fi
-
-# for model in 'MLP' 'LeNet' ; do 
-#     for approach in "${approaches[@]}"; do
-#         python3 ./create_storage.py --case $case --horizon $horizon --n_folds $nfolds --model $model --approach $approach --context_type $context_type 
-
-
-#         for ((id=0; id<$nfolds; id+=1)); do
-#                 echo 'case' $case 'model' $model 'horizon' $horizon 'nfolds' $nfolds 'CONTEXT_TYPE' $context_type 'TASK' $task 'APR' $approach 'ID' $id
-#                 sbatch --export=ALL,CASE=$case,MODEL=$model,HORIZON=$horizon,NFOLDS=$nfolds,CONTEXT_TYPE=$context_type,APR=$approach,ID=$id ./benchmark_launch.sh     
-#             done
-
-#     done
-# done
-
-
 
 
 for approach in "${approaches[@]}"; do
