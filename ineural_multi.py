18befd83a813ebb05e0ad3d516747ccc4e040574
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2024-01-30 09:26:56 -0500

    update ineural

--- ineural_multi.py
+++ ineural_multi.py
@@ -119,7 +119,7 @@ class INeurALmulti():
         
         for k in range(self.num_cls):
             f1_k, f2_k, dc_k = EE_forward(self.net1, self.net2, self.x_list[k])
-            u_k = f1_k + 1 / (t+1) * f2_k
+            u_k = f1_k + f2_k #1 / (t+1) *
             self.f1_list.append(f1_k)
             self.f2_list.append(f2_k)
             self.dc_list.append(dc_k)
@@ -176,8 +176,8 @@ class INeurALmulti():
                     self.y1.append(torch.Tensor([1 - reward]))
                     self.y2.append(torch.Tensor([1 - reward - self.f1_list[k] ]))
 
-        if action == 0 and (t>self.N):
-        # if (t<=50) or (t % 50 == 0 and t<1000 and t>50) or (t % 500 == 0 and t>=1000): 
+        # if action == 0 and (t>self.N):
+        if (t<=50) or (t % 50 == 0 and t<1000 and t>50) or (t % 500 == 0 and t>=1000): 
             self.train_NN_batch(self.net1, self.X1_train, self.y1)
             self.train_NN_batch(self.net2, self.X2_train, self.y2)
 
