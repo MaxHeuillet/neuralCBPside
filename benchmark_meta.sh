4589daaab149e231ecb869b8798ac733b44b273c
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2024-04-26 14:33:08 -0400

    update

--- benchmark_meta.sh
+++ benchmark_meta.sh
@@ -41,7 +41,7 @@ model='MLP'
 
 
 context_types=('adult' 'MagicTelescope' 'MNISTbinary' )
-approaches=( 'ineural3' 'ineural6' ) # 'cesa'  'margin' 'EEneuralcbpside_v6'  'neuronal3' 'neuronal6'  
+approaches=( 'ineural3' 'ineural6' 'cesa'  'margin' 'EEneuralcbpside_v6'  'neuronal3' 'neuronal6'  )  
 
 
 for context_type in "${context_types[@]}"; do
@@ -62,25 +62,26 @@ done
 ##### To evaluate on the 10-classes tasks with MLP
 #######################################
 
-case='case2'
-model='MLP'
+# case='case2'
+# model='MLP'
 
-context_types=( 'MNIST' 'FASHION' ) 
-approaches=( 'ineural3' 'ineural6' )  #'EEneuralcbpside_v6' 'neuronal3' 'neuronal6'
+# context_types=( 'MNIST' 'FASHION' ) 
+# approaches=( 'ineural3' 'ineural6' )  #'EEneuralcbpside_v6' 'neuronal3' 'neuronal6'
 
-for context_type in "${context_types[@]}"; do
+# for context_type in "${context_types[@]}"; do
 
-    for approach in "${approaches[@]}"; do
+#     for approach in "${approaches[@]}"; do
 
-        python3 ./create_storage.py --case $case --horizon $horizon --n_folds $nfolds --model $model --approach $approach --context_type $context_type 
+#         python3 ./create_storage.py --case $case --horizon $horizon --n_folds $nfolds --model $model --approach $approach --context_type $context_type 
 
-        for ((id=0; id<$nfolds; id+=1)); do
-            echo 'case' $case 'model' $model 'horizon' $horizon 'nfolds' $nfolds 'CONTEXT_TYPE' $context_type 'TASK' $task 'APR' $approach 'ID' $id
-            sbatch --export=ALL,CASE=$case,MODEL=$model,HORIZON=$horizon,NFOLDS=$nfolds,CONTEXT_TYPE=$context_type,APR=$approach,ID=$id ./benchmark_launch.sh     
-            done
+#         for ((id=0; id<$nfolds; id+=1)); do
+#             echo 'case' $case 'model' $model 'horizon' $horizon 'nfolds' $nfolds 'CONTEXT_TYPE' $context_type 'TASK' $task 'APR' $approach 'ID' $id
+#             sbatch --export=ALL,CASE=$case,MODEL=$model,HORIZON=$horizon,NFOLDS=$nfolds,CONTEXT_TYPE=$context_type,APR=$approach,ID=$id ./benchmark_launch.sh     
+#             done
 
-    done
-done
+#     done
+    
+# done
 
 
 
@@ -88,26 +89,26 @@ done
 ##### To evaluate on the other tasks with MLP
 #######################################
 
-case='game_case_seven'
-model='MLP'
+# case='game_case_seven'
+# model='MLP'
 
 
-context_types=('shuttle' 'covertype' )
-approaches=(  'ineural3' 'ineural6'  ) #'EEneuralcbpside_v6' 'neuronal3' 'neuronal6'
+# context_types=('shuttle' 'covertype' )
+# approaches=(  'ineural3' 'ineural6'  ) #'EEneuralcbpside_v6' 'neuronal3' 'neuronal6'
 
-for context_type in "${context_types[@]}"; do
+# for context_type in "${context_types[@]}"; do
 
-    for approach in "${approaches[@]}"; do
+#     for approach in "${approaches[@]}"; do
 
-        python3 ./create_storage.py --case $case --horizon $horizon --n_folds $nfolds --model $model --approach $approach --context_type $context_type 
+#         python3 ./create_storage.py --case $case --horizon $horizon --n_folds $nfolds --model $model --approach $approach --context_type $context_type 
 
-        for ((id=0; id<$nfolds; id+=1)); do
-            echo 'case' $case 'model' $model 'horizon' $horizon 'nfolds' $nfolds 'CONTEXT_TYPE' $context_type 'TASK' $task 'APR' $approach 'ID' $id
-            sbatch --export=ALL,CASE=$case,MODEL=$model,HORIZON=$horizon,NFOLDS=$nfolds,CONTEXT_TYPE=$context_type,APR=$approach,ID=$id ./benchmark_launch.sh     
-            done
+#         for ((id=0; id<$nfolds; id+=1)); do
+#             echo 'case' $case 'model' $model 'horizon' $horizon 'nfolds' $nfolds 'CONTEXT_TYPE' $context_type 'TASK' $task 'APR' $approach 'ID' $id
+#             sbatch --export=ALL,CASE=$case,MODEL=$model,HORIZON=$horizon,NFOLDS=$nfolds,CONTEXT_TYPE=$context_type,APR=$approach,ID=$id ./benchmark_launch.sh     
+#             done
 
-    done
-done
+#     done
+# done
 
 #######################################
 ##### To evaluate with LeNet on MNIST, Fashion-MNIST, CIFAR-10 
