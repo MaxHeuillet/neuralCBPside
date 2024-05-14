48a31fd24ffd904904ff128da7a92771662ffcfa
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2024-05-14 15:34:02 -0400

    update

--- README.md
+++ README.md
@@ -1,31 +1,20 @@
 # NeuralUCB
-This repository contains our pytorch implementation of NeuralUCB in the paper [Neural Contextual Bandits with UCB-based Exploration](https://arxiv.org/pdf/1911.04462.pdf) (accepted by ICML 2020). 
+This repository contains our pytorch implementation of NeuralCBP in the paper ''Neural Active Learning meets the Partial Monitoring Framework'' (accepted by UAI 2024). 
 
 ## Prerequisites: 
-* Pytorch and CUDA
-* future==0.18.2
-* joblib==0.15.1
-* numpy==1.18.1
-* pkg-resources==0.0.0
-* scikit-learn==0.22.1
-* scipy==1.4.1
-* torch==1.5.0
-
-## Usage:
-Use python to run train.py for experiments.
-
-## Command Line Arguments:
-* --size: bandit algorithm time horizon
-* --dataset: datasets
-* --shuffle: to shuffle the dataset or not
-* --seed: random seed for shuffle
-* --nu: nu for control variance
-* --lambda: lambda for regularization
-* --hidden: network hidden size
-
-
-## Usage Examples:
-* Run experiments on [mnist](http://yann.lecun.com/exdb/publis/pdf/lecun-98.pdf):
 ```bash
-  -  python3 train.py --nu 0.00001 --lamdba 0.00001 --dataset mnist
+  pip install -r requirements.txt
 ```
+
+## Load datasets:
+
+```bash
+  python3 load_data.py
+```
+
+## Launch experiment on the cluster:
+
+```bash
+  bash benchmark_meta.sh
+```
+
--- benchmark_meta.sh
+++ benchmark_meta.sh
@@ -136,41 +136,7 @@ for context_type in "${context_types[@]}"; do
 done
 
 
-#######################################
-##### To evaluate on all of them:
-#######################################
 
-# horizon=9999
-# nfolds=$1
-# context_type=$2
-# case=$3
-# model=$4
-
-# if [ "$model" == "MLP" ]; then
-#     if [ "$case" == "case1" ]; then
-#         approaches=('ineural3' 'ineural6' ) #'margin' 'cesa'  'neuronal3' 'neuronal6' 'EEneuralcbpside_v6'
-#     else
-#         approaches=('ineural3' 'ineural6' ) #'neuronal3' 'neuronal6' 'EEneuralcbpside_v6'
-#     fi
-# else
-#     if [ "$case" == "case1" ]; then
-#         approaches=('neuronal3' 'neuronal6' 'EEneuralcbpside_v6')
-#     else
-#         approaches=('neuronal3' 'neuronal6' 'EEneuralcbpside_v6')
-#     fi
-# fi
-
-
-# for approach in "${approaches[@]}"; do
-#     python3 ./create_storage.py --case $case --horizon $horizon --n_folds $nfolds --model $model --approach $approach --context_type $context_type 
-
-
-#     for ((id=0; id<$nfolds; id+=1)); do
-#         echo 'case' $case 'model' $model 'horizon' $horizon 'nfolds' $nfolds 'CONTEXT_TYPE' $context_type 'TASK' $task 'APR' $approach 'ID' $id
-#         sbatch --export=ALL,CASE=$case,MODEL=$model,HORIZON=$horizon,NFOLDS=$nfolds,CONTEXT_TYPE=$context_type,APR=$approach,ID=$id ./benchmark_launch.sh     
-#         done
-
-# done
 
 
 
